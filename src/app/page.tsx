e0efca80a4ddfd299a01c38a118abdb34a7c6475
wlals <jiminelp@gmail.com>
2024-02-15 21:27:20 +0900

    📝:: sideBar Import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Header from "@/components/header";
+import Header from "@/components/header/header";
+import SideBar from "@/components/sideBar/sideBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-start">
       <Header />
+      <SideBar />
     </main>
   );
 }
