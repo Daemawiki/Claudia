9142a70ce8ae5fb25cc5be2b2490a5e9e812630b
wlals <jiminelp@gmail.com>
2024-02-15 21:28:07 +0900

    ✨:: sideBar 컴포넌트 퍼블리싱

---
+++ src/components/sideBar/sideBar.tsx
@@ -0,0 +1,47 @@
+import DocumentPreview from "./documentPreview";
+
+const SideBar = () => {
+  return (
+    <div className="flex flex-col gap-10  w-[300px] mt-5">
+      <div className="bg-white rounded-2xl">
+        <div className="px-5 py-[10px] border-b">
+          <span className="font-bold text-base">최근 변경된 문서</span>
+        </div>
+        <DocumentPreview documentName="박자민" time={456} views={54} />
+        <DocumentPreview documentName="박지민" time={456} views={5948} />
+        <DocumentPreview documentName="박지미" time={789} views={3} />
+        <DocumentPreview documentName="빡지민" time={6155} views={1004} />
+        <DocumentPreview documentName="박시민" time={6155} views={7942} />
+        <DocumentPreview documentName="부지민" time={6155} views={1592} />
+        <DocumentPreview documentName="반지민" time={6155} views={5877} />
+        <DocumentPreview documentName="박지마" time={6155} views={4485} />
+        <DocumentPreview documentName="보지민" time={6155} views={6974} />
+        <DocumentPreview documentName="박주민" time={6155} views={32} />
+        <div className="px-5 py-[10px] flex justify-end">
+          <span>더보기</span>
+        </div>
+      </div>
+
+      <div className="bg-white rounded-2xl">
+        <div className="px-5 py-[10px] border-b">
+          <span className="font-bold text-base">인기 TOP 10</span>
+        </div>
+        <DocumentPreview documentName="박시민" time={6155} views={7942} />
+        <DocumentPreview documentName="보지민" time={6155} views={6974} />
+        <DocumentPreview documentName="박지민" time={456} views={5948} />
+        <DocumentPreview documentName="반지민" time={6155} views={5877} />
+        <DocumentPreview documentName="박지마" time={6155} views={4485} />
+        <DocumentPreview documentName="부지민" time={6155} views={1592} />
+        <DocumentPreview documentName="빡지민" time={6155} views={1004} />
+        <DocumentPreview documentName="박자민" time={456} views={54} />
+        <DocumentPreview documentName="박주민" time={6155} views={32} />
+        <DocumentPreview documentName="박지미" time={789} views={3} />
+        <div className="px-5 py-[10px] flex justify-end">
+          <span>더보기</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
