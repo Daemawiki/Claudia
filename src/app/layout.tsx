161abd5dbb6a9218edf0b29ace3f731da179ba70
wlals <jiminelp@gmail.com>
2024-02-23 05:34:29 +0900

    💄:: 폰트 import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "./ReactQueryProvider";
 import StoreProvider from "./StoreProvider";
 
-const sans = Noto_Sans({ subsets: ["latin"] });
+const sans = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
