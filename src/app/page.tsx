117e6ed5184098cdce5784d03481d24668016670
wlals <jiminelp@gmail.com>
2024-02-27 01:24:41 +0900

    ✨:: 목차 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
+import Index from "@/components/index";
 import SideBar from "@/components/sideBar/sideBar";
 import Table from "@/components/table/table";
 import Title from "@/components/title/title";
+import { mainPageIndex } from "@/constant/indexItem";
 
 export default function Main() {
   return (
@@ -12,6 +14,9 @@ export default function Main() {
         <div className="flex w-[1000px] bg-white min-h-screen flex-col gap-[60px] px-[100px] py-[60px]">
           <Title title="대마위키" lastModifiedTime={20240217} />
           <Table />
+          <div>
+            <Index index={mainPageIndex} />
+          </div>
           <Footer />
         </div>
         <SideBar />
