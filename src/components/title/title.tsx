759eb9b69ad426a96b24ad5957259ffc18e55f91
wlals <jiminelp@gmail.com>
2024-02-27 01:24:41 +0900

    ✨:: 목차 구현

---
+++ src/components/title/title.tsx
@@ -0,0 +1,18 @@
+interface TitleProps {
+  title: string;
+  additionalInfo?: string;
+  lastModifiedTime?: number;
+}
+
+const Title = ({ title, additionalInfo, lastModifiedTime }: TitleProps) => {
+  let subText = additionalInfo || "";
+  if (lastModifiedTime) subText = `최근 수정 시각 : ${lastModifiedTime}`;
+  return (
+    <div className="flex justify-between items-end">
+      <span className="font-bold text-[48px]">{title}</span>
+      <span className="text-sm text-[#BDBDBD]">{subText}</span>
+    </div>
+  );
+};
+
+export default Title;
