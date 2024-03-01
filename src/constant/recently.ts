c291545efa91509590c939283f8f9829ba60ce71
wlals <jiminelp@gmail.com>
2024-03-02 00:06:19 +0900

    📝:: 최근 변경 더미값

---
+++ src/constant/recently.ts
@@ -0,0 +1,202 @@
+export const recentlyItem = [
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+  {
+    type: "CREATE", // UPDATE DELETE CREATE
+    documentId: "65debf3ddb62b43098789280",
+    title: "나는 바보 멍청이 김승원이다.", // 문서 이름
+    editor: {
+      user: "김승원",
+      id: "65deb06929497f02efdd7e2a",
+    },
+    createdDateTime: "2024-02-28 14:06:05",
+  },
+];
