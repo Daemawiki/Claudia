079e3264adaaf4753ba9d009cf7568dfae525643
wlals <jiminelp@gmail.com>
2024-03-02 22:43:49 +0900

    ✨:: 인기 페이지 퍼블리싱

--- src/app/popular/page.tsx
+++ src/app/popular/page.tsx
@@ -2,14 +2,39 @@ import Header from "@/components/header/header";
 import Title from "@/components/title/title";
 import Footer from "@/components/footer/footer";
 import SideBar from "@/components/sideBar/sideBar";
+import Standard from "@/components/reserch/standard";
+import Preview from "@/components/reserch/preview";
+import { recentlyItem } from "@/constant/recently";
 
 export default function Popular() {
+  const renderPreview = () => {
+    const items = recentlyItem;
+    return items.map(item => {
+      return (
+        <Preview
+          title={item.title}
+          editor={item.editor.user}
+          editTime={item.createdDateTime}
+          review={200}
+        />
+      );
+    });
+  };
   return (
     <>
       <Header />
       <main className="flex min-h-screen justify-center flex-wrap gap-5">
         <div className="flex w-[1000px] bg-white min-h-screen flex-col gap-[20px] px-[100px] py-[60px]">
           <Title title="인기" />
+          <div className="flex flex-col gap-[10px]">
+            <div className="flex gap-[10px]">
+              <Standard title="항목" width={"grow"} />
+              <Standard title="조회수" width={150} />
+              <Standard title="변경자" width={220} />
+              <Standard title="변경 시간" width={150} />
+            </div>
+            {renderPreview()}
+          </div>
           <Footer />
         </div>
         <SideBar />
