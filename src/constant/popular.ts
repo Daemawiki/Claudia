2be7b9cd8da42ac88649113db56d1298729f2979
wlals <jiminelp@gmail.com>
2024-03-03 22:53:23 +0900

    📝:: popular 더미 데이터 추가

---
+++ src/constant/popular.ts
@@ -0,0 +1,18 @@
+const popularItem = [
+  {
+    id: "65dd8a9ed092a46e07b6263e",
+    title: "김승원",
+    numberOfUpdate: 12,
+    updatedDate: "2024-02-27 16:09:18",
+    view: 4,
+  },
+  {
+    id: "65ddc90febac10515c326040",
+    title: "나는 바보 멍청이 김승원이다.",
+    numberOfUpdate: 0,
+    updatedDate: "2024-02-27 20:35:43",
+    view: 4,
+  },
+];
+
+export default popularItem;
