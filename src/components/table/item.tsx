8603ad056422a7a707da2b9438e0b2e7d718b6fe
wlals <jiminelp@gmail.com>
2024-03-10 23:12:02 +0900

    ✨:: info 삭제 기능 퍼블리싱

--- src/components/table/item.tsx
+++ src/components/table/item.tsx
@@ -1,14 +1,22 @@
+"use client";
 import linkChecker from "@/utils/function/linkCheck";
-import React from "react";
+import React, { CSSProperties, useEffect, useState } from "react";
 import LinkBlock from "./linkBlock";
 import formatText from "@/utils/function/formatText";
+import { Info } from "@/constant/documentType";
 
 interface TableItemProps {
   name: string;
   value: string;
+  del?: boolean;
+  setValue?: React.Dispatch<React.SetStateAction<Info[] | undefined>>;
+  index: number;
 }
 
-const TableItem = ({ name, value }: TableItemProps) => {
+const TableItem = ({ name, value, del, setValue, index }: TableItemProps) => {
+  const [containerStyle, setContainerStyle] = useState<CSSProperties>({
+    display: "flex",
+  });
   const lines = value.split("</p><p>");
   const formattedValue = lines.map((line, index) => {
     const delpTag = line.replace("<p>", "").replace("</p>", "");
@@ -39,9 +47,36 @@ const TableItem = ({ name, value }: TableItemProps) => {
     );
   });
 
+  const delClickHandler = () => {
+    setContainerStyle({
+      display: "none",
+    });
+    setValue &&
+      setValue(prev => {
+        if (prev) {
+          return prev?.filter((_, i) => {
+            return i !== index;
+          });
+        }
+      });
+  };
+
   return (
-    <div className="flex flex-col min-w-[150px] min-h-[100px] gap-[8px]">
-      <span className="font-medium text-2xl text-[#93DF3F]">{name}</span>
+    <div
+      className="flex flex-col min-w-[150px] min-h-[100px] gap-[8px] relative"
+      style={containerStyle}
+    >
+      <div className="font-medium text-2xl text-[#93DF3F] flex justify-between w-full">
+        <span>{name}</span>
+        {del && (
+          <button
+            className="w-6 h-6 text-center font-bold"
+            onClick={delClickHandler}
+          >
+            <div className="w-[14px] border-none h-[2px] bg-red-500 rounded-[5px]"></div>
+          </button>
+        )}
+      </div>
       <span className="font-medium text-base flex gap-[8px] flex-col">
         {formattedValue}
       </span>
