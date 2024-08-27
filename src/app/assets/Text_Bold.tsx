c5c0b0f7f12e6ac57438a5a0d2204a489815e249
ojinikim <ojinikim@gmail.com>
2024-08-27 23:47:10 +0900

    fix :: svg 속성 이름 변경

--- src/app/assets/Text_Bold.tsx
+++ src/app/assets/Text_Bold.tsx
@@ -4,7 +4,11 @@ interface PropsType {
   onClick?: () => void;
 }
 
-export const Text_Bold = ({ size = 24, onClick, className = "" }: PropsType) => {
+export const Text_Bold = ({
+  size = 24,
+  onClick,
+  className = "",
+}: PropsType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,9 +21,9 @@ export const Text_Bold = ({ size = 24, onClick, className = "" }: PropsType) => 
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M9.333 14.5h5.25m-5.25 0V6.333h5.25a4.083 4.083 0 1 1 0 8.167m-5.25 0v8.166h6.417a4.083 4.083 0 1 0 0-8.166h-1.167"
       />
     </svg>
--- src/app/assets/Text_Italic.tsx
+++ src/app/assets/Text_Italic.tsx
@@ -4,7 +4,11 @@ interface PropsType {
   onClick?: () => void;
 }
 
-export const Text_Italic = ({ size = 24, onClick, className = "" }: PropsType) => {
+export const Text_Italic = ({
+  size = 24,
+  onClick,
+  className = "",
+}: PropsType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,9 +21,9 @@ export const Text_Italic = ({ size = 24, onClick, className = "" }: PropsType) =
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M9.333 22.666h2.333m0 0H14m-2.334 0 4.667-16.333m-2.333 0h2.333m0 0h2.333"
       />
     </svg>
