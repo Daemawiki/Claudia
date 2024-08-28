48ab2e862c3da9671b2dfe8a5154fbd485d61408
ojinikim <ojinikim@gmail.com>
2024-08-28 16:14:00 +0900

    add :: 기존 버튼 삭제, 버튼 컴포넌트 병합

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,21 +3,34 @@ import React from "react";
 interface ButtonProps {
   text: string;
   style?: "primary" | "primary2" | "white";
+  state?: "enabled" | "disabled";
   onClick?: () => void;
+  big?: boolean;
 }
 
-export const Button = ({ text, onClick, style = "primary" }: ButtonProps) => {
+export const Button = ({
+  text,
+  onClick,
+  style = "primary",
+  state = "enabled",
+  big,
+}: ButtonProps) => {
   const buttonStyle = {
-    primary: "text-lime500 bg-white hover:bg-gray50",
-    primary2: "text-white bg-lime500 hover:bg-lime600",
-    white: "bg-white text-gray800 hover:bg-gray100",
+    primary: { enabled: "text-lime500 bg-white hover:bg-gray50", disabled: "" },
+    primary2: {
+      enabled: "text-white bg-lime500 hover:bg-lime600",
+      disabled: "",
+    },
+    white: { enabled: "", disabled: "" },
   };
   return (
-    <button
-      onClick={onClick}
-      className={`transition-all text-semibold16 flex px-3 py-2 rounded-md ${buttonStyle[style]}`}
-    >
-      {text}
-    </button>
+    <>
+      <button
+        onClick={onClick}
+        className={`transition-all justify-center items-center flex  ${big ? "rounded-lg p-4 text-semibold18" : "px-3 py-2 rounded-md text-semibold16"} ${buttonStyle[style][state]}`}
+      >
+        {text}
+      </button>
+    </>
   );
 };
