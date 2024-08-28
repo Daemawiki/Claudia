4240b5d4465f7c5829986975250b7210676ebb32
ojinikim <ojinikim@gmail.com>
2024-08-28 17:18:24 +0900

    chore :: header 불러오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,11 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header/header";
-import Index from "@/components/index";
-import SideBar from "@/components/sideBar/sideBar";
-import Table from "@/components/table/table";
-import Title from "@/components/title/title";
-import Lesson from "@/components/lesson/lesson";
-import { mainPageIndex } from "@/constant/indexItem";
-import { mainTableItem } from "@/constant/tableItem";
-import "react-quill/dist/quill.snow.css";
-import Header2 from "@/components/header/Header2";
-import EditModal from "@/components/edit/EditModal";
+import { Header, Footer } from "@/components";
+import Document from "./document/[id]/page";
 
-export default function Main() {
-  const index = mainPageIndex;
-  const renderContent = () => {
-    return index.map(item => {
-      return (
-        <Lesson
-          title={item.title}
-          index={item.index}
-          content={item.detail}
-          key={item.index}
-        />
-      );
-    });
-  };
+export default function Home() {
   return (
     <>
-      <Header2 />
-      <EditModal />
-      {/* <Header />
-      <main className="flex min-h-screen justify-center flex-wrap gap-5">
-        <div className="flex w-[1000px] bg-white min-h-screen flex-col gap-[60px] px-[100px] py-[60px]">
-          <Title title="대마위키" lastModifiedTime={"2023-11-12 20:56:08"} />
-          <Table
-            info={mainTableItem}
-            name="대마위키"
-            subName="대덕소프트웨어마이스터고등학교의 위키"
-          />
-          <div className="flex flex-col gap-[10px]">
-            <Index index={index} />
-            {renderContent()}
-          </div>
-          <Footer />
-        </div>
-        <SideBar />
-      </main> */}
+      <Document />
+      <Footer />
     </>
   );
 }
