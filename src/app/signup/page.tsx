62b97178b74a90bb41236be3b98a6fa4be9720c1
ojinikim <ojinikim@gmail.com>
2024-08-28 17:22:53 +0900

    add :: 회원가입 페이지 퍼블리싱

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,75 +1,84 @@
 "use client";
-import Button from "@/components/button";
-import Header from "@/components/header/header";
-import Title from "@/components/title/title";
-import Input from "@/components/input/input";
-import SideBar from "@/components/sideBar/sideBar";
-import Dropdown from "@/components/input/dropdown";
-import { majorMenu, periodMenu } from "@/constant/dropdownItem";
-import PasswordForm from "@/components/input/passwordForm";
-import Link from "next/link";
-import { SetStateAction } from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Arrow } from "../assets";
+import { Button, RegisterInput } from "@/components";
+import { useRouter } from "next/navigation";
+import { Email, EmailVerification, Name, Password } from "./Register";
 
-export default function SignUp() {
-  const [gen, setGen] = useState<string>("");
-  const [major, setMajor] = useState<string>("");
+export default function Signup() {
+  const router = useRouter();
+  const [pageNum, setPageNum] = useState<number>(0);
+  const page = [
+    {
+      page: <Email />,
+      details: "이메일 인증을 위해 DSM 이메일을 입력해주세요",
+      buttonText: "인증번호 전송",
+    },
+    {
+      page: <EmailVerification />,
+      details: "hamster@dsm.hs.kr 로 인증번호를 전송했습니다",
+      buttonText: "확인",
+    },
+    {
+      page: <Password />,
+      details: "비밀번호를 설정해주세요",
+      buttonText: "확인",
+    },
+    {
+      page: <Name />,
+      details: "이름을 입력해주세요",
+      buttonText: "확인",
+    },
+  ];
+
+  const prevStep = () => {
+    if (pageNum > 0) {
+      setPageNum(pageNum - 1);
+    } else {
+      router.back();
+    }
+  };
+  const nextStep = () => {
+    if (pageNum < 3) {
+      setPageNum(pageNum + 1);
+    }
+  };
+
   return (
-    <>
-      <main className="flex min-h-screen justify-center flex-wrap gap-5">
-        <div className="flex w-[1000px] bg-white min-h-screen flex-col gap-[20px] px-[100px] py-[60px]">
-          <Title title="회원가입" />
-          <div className="flex flex-col gap-[10px]">
-            <Input title="이름" width={"full"} />
-            <Input
-              title="DSM 이메일"
-              width={"full"}
-              buttonText={"인증"}
-              buttonFontSize={14}
-              buttonStyle={{ position: "absolute", right: "10px" }}
-            />
-            <Input
-              title="인증코드"
-              width={"full"}
-              isNumber
-              buttonText="확인"
-              buttonFontSize={14}
-              buttonStyle={{ position: "absolute", right: "10px" }}
-            />
-            <Dropdown
-              title="기수"
-              content={periodMenu}
-              width={110}
-              setForm={function (value: SetStateAction<string>): void {
-                throw new Error("Function not implemented.");
-              }}
-            />
-            <Dropdown
-              title="전공"
-              content={majorMenu}
-              width={180}
-              setForm={function (value: SetStateAction<string>): void {
-                throw new Error("Function not implemented.");
-              }}
-            />
-            <PasswordForm />
-            <div className="flex justify-end items-end gap-[10px] pt-5">
-              <Link href={"/login"}>
-                <Button
-                  text="로그인"
-                  color="black"
-                  width={139}
-                  height={48}
-                  fontSize={20}
-                  rounded={16}
-                />
-              </Link>
-              <Button text="회원가입" width={139} height={48} rounded={16} />
-            </div>
-          </div>
+    <div className="w-full h-screen flex justify-center pt-24">
+      <div className="w-[480px] flex flex-col gap-12 p-6 rounded-3xl">
+        <div
+          onClick={() => prevStep()}
+          className="rounded-md w-fit border border-gray200 p-2 flex bg-white hover:bg-gray50"
+        >
+          <Arrow size={28} className="text-gray600" />
         </div>
-        <SideBar />
-      </main>
-    </>
+        <div className="w-full flex flex-col gap-6">
+          <div className="w-full flex flex-col gap-3">
+            <p className="text-bold36 text-black">회원가입</p>
+            <p className="text-gray500 text-medium18">
+              {page[pageNum].details}
+            </p>
+          </div>
+          {page[pageNum].page}
+        </div>
+        <div className="w-full flex flex-col gap-6">
+          <div className="w-full gap-2 flex justify-center items-center">
+            {page.map((item, index) => (
+              <div
+                key={index}
+                className={`h-2 transition-all rounded-full ${pageNum === index ? "w-4 bg-lime400" : "w-2 bg-gray300"}`}
+              />
+            ))}
+          </div>
+          <Button
+            big
+            onClick={() => nextStep()}
+            style="primary2"
+            text={page[pageNum].buttonText}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
