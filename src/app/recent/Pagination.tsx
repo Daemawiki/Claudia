18e3876fc608c6bc1ef865f52b67bbcc5375e684
ojinikim <ojinikim@gmail.com>
2024-08-29 23:09:02 +0900

    add :: 최근변경 페이지 퍼블리싱

---
+++ src/app/recent/Pagination.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Arrow } from "../assets";
+
+export const Pagination = () => {
+  const arr = [1, 2, 3, 4, 5];
+  return (
+    <div className="w-full flex justify-center gap-7 px-6">
+      <div className="flex cursor-pointer p-1.5 rounded-lg hover:bg-gray100">
+        <Arrow className="text-gray700" />
+      </div>
+      <div className="flex items-center">
+        {arr.map((item, index) => (
+          <div className=" cursor-pointer rounded-lg h-9 w-9 flex items-center justify-center hover:bg-lime50">
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className={`flex cursor-pointer p-1.5 rounded-lg hover:bg-gray100`}>
+        <Arrow direction="right" className="text-gray700" />
+      </div>
+    </div>
+  );
+};
