4834f189712b0c72154cbcab831f1dadfa910051
ojinikim <ojinikim@gmail.com>
2024-08-30 11:51:53 +0900

    add :: 분류 메인페이지 퍼블리싱

--- src/app/division/page.tsx
+++ src/app/division/page.tsx
@@ -1,5 +1,4 @@
-import Header from "@/components/Header";
-import Title from "@/components/title/title";
+import { Title } from "../document/[id]/Title";
 // import Footer from "@/components/Footer";
 import SideBar from "@/components/sideBar/sideBar";
 import Preview from "@/components/reserch/preview";
@@ -7,15 +6,8 @@ import Form from "@/components/division/form";
 
 export default function Division() {
   return (
-    <>
-      <main className="flex min-h-screen justify-center flex-wrap gap-5">
-        <div className="flex w-[1000px] bg-white min-h-screen flex-col gap-[60px] px-[100px] py-[60px]">
-          <Title title="분류" />
-          <Form />
-          {/* <Footer /> */}
-        </div>
-        <SideBar />
-      </main>
-    </>
+    <div className="w-full flex justify-center pb-12">
+      <div className="w-full pt-16 px-12 max-w-[1200px] flex flex-col gap-14"></div>
+    </div>
   );
 }
