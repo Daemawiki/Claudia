c1909bec8bbaf6f3f26d29f0a50c0fb6cee74b5b
ojinikim <ojinikim@gmail.com>
2024-09-02 19:12:24 +0900

    edit :: 분류 메인페이지 카드 컴포넌트 분리

--- src/app/division/page.tsx
+++ src/app/division/page.tsx
@@ -1,13 +1,25 @@
 import { Title } from "../document/[id]/Title";
-// import Footer from "@/components/Footer";
-import SideBar from "@/components/sideBar/sideBar";
-import Preview from "@/components/reserch/preview";
-import Form from "@/components/division/form";
+import { Card } from "./Card";
+import StudentPage from "./StudentPage";
 
 export default function Division() {
   return (
     <div className="w-full flex justify-center pb-12">
-      <div className="w-full pt-16 px-12 max-w-[1200px] flex flex-col gap-14"></div>
+      <div className="w-full pt-16 px-12 max-w-[1200px] flex flex-col gap-14">
+        <Title
+          noPadding
+          noShow
+          title="분류"
+          group="대마위키"
+          details="카테고리"
+        />
+        <div className="w-full flex-wrap gap-6 py-12 flex">
+          <Card longWidth type="student" />
+          <Card type="teacher" />
+          <Card type="accident" />
+          <Card type="club" />
+        </div>
+      </div>
     </div>
   );
 }
