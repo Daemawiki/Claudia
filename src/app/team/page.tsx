6bfe7d741eeea660fdf420b9ee4d817e2ae7478e
ojinikim <ojinikim@gmail.com>
2024-10-09 23:49:10 +0900

    add :: 팀 소개 카드 퍼블리싱

---
+++ src/app/team/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import { Arrow } from "../assets";
+import { Button, RegisterInput } from "@/components";
+import { useRouter } from "next/navigation";
+import { MemberCard } from "./MemberCard";
+
+export default function Team() {
+  const router = useRouter();
+  return (
+    <div className="w-full flex justify-center px-6">
+      <div className="w-full max-w-[1200px] flex flex-col pt-16">
+        <div className="w-full px-12 py-24 gap-6 flex flex-col">
+          <p className="text-bold40">
+            대마위키를 만든
+            <br />
+            사람들을 소개합니다
+          </p>
+          <p className="text-medium20 text-gray500">
+            팀 DM은 대덕소프트웨어마이스터고등학교 학생들을 위한 <br />
+            위키 서비스, 대마위키를 기획하였습니다.
+          </p>
+        </div>
+        <div className="w-full px-12 py-24 gap-12 flex flex-col">
+          <div className="flex flex-col gap-6">
+            <p className="text-lime500 text-semibold18">팀 DM 멤버 소개</p>
+            <p className="text-bold40">저희를 소개합니다</p>
+            <p className="text-medium20 text-gray500">
+              하도 이상한 일이 많이 일어나는 대마고, 부마위키를 보고
+              <br />
+              우리도 이런 위키를 만들어서 박제해버리면 어떨까? 하는 생각을 가진
+              네명이 모여 만들었습니다.
+            </p>
+          </div>
+          <div className="w-full flex flex-wrap gap-2">
+            <MemberCard />
+            <MemberCard />
+            <MemberCard />
+            <MemberCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
