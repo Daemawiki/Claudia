91c0d2f9d622ea68326257539a85ce520c58625b
ojinikim <ojinikim@gmail.com>
2024-10-10 17:21:01 +0900

    chore :: 팀원 정보로 팀소개카드 map돌리기

--- src/app/team/MemberCard.tsx
+++ src/app/team/MemberCard.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Github } from "../assets";
 
-export const MemberCard = () => {
+interface CardProps {
+  name?: string;
+  img?: string;
+  github?: string;
+  major?: string;
+}
+
+export const MemberCard = ({ name, img, github, major }: CardProps) => {
   return (
     <div className="group rounded-lg w-full max-w-[270px] h-[360px] relative overflow-hidden">
       <div className="absolute opacity-0 transition-all group-hover:opacity-100 z-10 self-center left-2.5 right-2.5 bottom-2.5 flex flex-col gap-1 p-4 rounded-md bg-white">
         <div className="w-full justify-between items-center flex">
-          <p className="text-semibold20">김어진</p>
-          <a href="" className="cursor-pointer">
+          <p className="text-semibold20">{name}</p>
+          <a href={github} className="cursor-pointer">
             <Github />
           </a>
         </div>
-        <p className="text-gray400 text-medium16">Frontend · Design</p>
+        <p className="text-gray400 text-medium16">{major}</p>
       </div>
       <img
-        src="https://image.dongascience.com/Photo/2019/12/fb4f7da04758d289a466f81478f5f488.jpg"
+        src={img}
         className="absolute w-full h-full grayscale group-hover:grayscale-0 transition-all"
       />
     </div>
