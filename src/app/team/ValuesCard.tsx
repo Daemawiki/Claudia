04b587683e57056a9af47d68b916bf05a034d63d
wlals <jiminelp@gmail.com>
2024-10-11 09:58:25 +0900

    add :: 팀 소개 페이지 퍼블리싱

---
+++ src/app/team/ValuesCard.tsx
@@ -0,0 +1,17 @@
+interface PropsType {
+  index: number;
+  title: string;
+  details: string;
+}
+
+export const ValuesCard = ({ index, title, details }: PropsType) => {
+  return (
+    <div className="border-gray200 border-[1px] rounded-2xl flex flex-col gap-4 p-6">
+      <div className="flex w-6 h-6 justify-center items-center rounded-md bg-lime50">
+        <p className="text-lime500">{index}</p>
+      </div>
+      <p className="text-semibold24">{title}</p>
+      <p className="text-medium16 text-gray500">{details}</p>
+    </div>
+  );
+};
