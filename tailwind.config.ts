3247d8717071cd472e7237685ce1ab9f32a59b14
wlals <jiminelp@gmail.com>
2024-10-11 09:58:25 +0900

    add :: 팀 소개 페이지 퍼블리싱

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -208,9 +208,14 @@ module.exports = {
         "0%, 100%": { transform: "scale(1)" },
         "50%": { transform: "scale(1.25)" },
       },
+      fadeIn: {
+        "0%": { opacity: "0", transform: "translateY(50px)" },
+        "100%": { opacity: "1", transform: "translateY(0px)" },
+      },
     },
     animation: {
       bouncing: "bouncing 0.5s ease-in-out",
+      fadeIn: "fadeIn 0.5s linear",
     },
   },
   plugins: [],
