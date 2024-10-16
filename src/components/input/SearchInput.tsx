c7e33e12bd95dd73182718ac99801a805c12585d
ojinikim <ojinikim@gmail.com>
2024-10-16 23:28:34 +0900

    fix :: 검색어 입력창 높이가 눌려서 min-height 추가

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -7,7 +7,7 @@ interface InputProps {
 
 export const SearchInput = ({ placeholder }: InputProps) => {
   return (
-    <div className="flex focus-within:bg-white transition-all px-3 py-2 items-center justify-between w-full gap-2 overflow-hidden border rounded-md bg-gray50 border-gray200">
+    <div className="flex focus-within:bg-white min-h-9 transition-all px-3 py-2 items-center w-full gap-2 overflow-hidden border rounded-md bg-gray50 border-gray200">
       <input
         placeholder={placeholder}
         className="text-black placeholder:text-gray400 bg-transparent text-medium16 w-full"
