c4ef69ab6bcb27f9a0796842ffd81728755e27ac
wlals <jiminelp@gmail.com>
2024-10-17 18:59:22 +0900

    feat :: 입력 창 퍼블리싱 약간

--- src/app/(without-header)/document/[id]/edit/page.tsx
+++ src/app/(without-header)/document/[id]/edit/page.tsx
@@ -1,27 +1,19 @@
+import { Close, Document } from "@/app/assets";
+import { Button } from "@/components";
+
 const Edit = () => {
   return (
-    <div>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
-      <p>
-        저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장저장
-      </p>
+    <div className="flex flex-col w-full">
+      <div className="flex justify-between">
+        <Close className="text-gray600 w-6" />
+        <Button style="primary2" text="저장">
+          <Document />
+        </Button>
+      </div>
+      <div className="flex">
+        <div className="flex">사이드바</div>
+        <textarea></textarea>
+      </div>
     </div>
   );
 };
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface ButtonProps {
-  text: string;
+  text?: string;
   style?: "primary" | "primary2" | "white";
   state?: "enabled" | "disabled";
   onClick?: () => void;
   big?: boolean;
+  children?: Readonly<React.ReactNode>;
 }
 
 export const Button = ({
@@ -14,6 +15,7 @@ export const Button = ({
   style = "primary",
   state = "enabled",
   big,
+  children,
 }: ButtonProps) => {
   const buttonStyle = {
     primary: { enabled: "text-lime500 bg-white hover:bg-gray50", disabled: "" },
@@ -27,9 +29,10 @@ export const Button = ({
     <>
       <button
         onClick={onClick}
-        className={`transition-all justify-center items-center flex  ${big ? "rounded-lg p-4 text-semibold18" : "px-3 py-2 rounded-md text-semibold16"} ${buttonStyle[style][state]}`}
+        className={`transition-all justify-center items-center gap-1 flex  ${big ? "rounded-lg p-4 text-semibold18" : "px-3 py-2 rounded-md text-semibold16"} ${buttonStyle[style][state]}`}
       >
         {text}
+        {children}
       </button>
     </>
   );
