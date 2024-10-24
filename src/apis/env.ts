4ffefcf8c70c051f9de029cf1725a35939afcd53
ojinikim <ojinikim@gmail.com>
2024-10-24 14:57:46 +0900

    edit :: BASE_URL로 변경

--- src/apis/env.ts
+++ src/apis/env.ts
@@ -5,5 +5,5 @@ const isStag = window.location.href.includes("stag");
 
 export const COOKIE_DOMAIN = isLocalhost ? "localhost" : "daemawiki.xquare.app";
 
-export const MAIN_URL = process.env.BASE_URL;
+export const BASE_URL = process.env.BASE_URL;
 export const SERVER_URL = process.env.SETVER_URL;
