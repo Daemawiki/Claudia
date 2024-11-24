7e7ae62b04d206dfac19ab451c802476695688a3
ojinikim <ojinikim@gmail.com>
2024-11-24 23:45:56 +0900

    edit :: 도메인 변경

--- src/apis/env.ts
+++ src/apis/env.ts
@@ -3,7 +3,7 @@ import process from "process";
 const isLocalhost = window.location.href.includes("localhost");
 const isStag = window.location.href.includes("stag");
 
-export const COOKIE_DOMAIN = isLocalhost ? "localhost" : "daemawiki.xquare.app";
+export const COOKIE_DOMAIN = isLocalhost ? "localhost" : "daemawiki-server.xquare.app";
 
 export const BASE_URL = process.env.BASE_URL;
 export const SERVER_URL = process.env.SETVER_URL;
