cdaaf86f31efb65a426be4315a2842d7e2290513
ojinikim <ojinikim@gmail.com>
2024-11-27 23:44:53 +0900

    feat :: Toast 알림 추가 및 상태 관리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,45 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "./ReactQueryProvider";
 import StoreProvider from "./StoreProvider";
-import { Header } from "@/components";
+import { Toast } from "@/components";
+import { useToastManager, ToastContext } from "@/components";
 
 const sans = Noto_Sans_KR({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "대마위키",
-  description: "우리의 학교생활 이야기",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const { toasts, addToast, removeToast } = useToastManager();
+
   return (
     <html lang="en">
       <body className={sans.className}>
         <StoreProvider>
-          <ReactQueryProvider>{children}</ReactQueryProvider>
+          <ReactQueryProvider>
+            <div>
+              {/* Toast UI */}
+              <div className="fixed top-4 right-4 space-y-4">
+                {toasts.map(toast => (
+                  <Toast
+                    key={toast.id}
+                    type={toast.type}
+                    message={toast.message}
+                    onClose={() => removeToast(toast.id)}
+                  />
+                ))}
+              </div>
+
+              {/* Context로 addToast 공유 */}
+              <ToastContext.Provider value={{ addToast }}>
+                {children}
+              </ToastContext.Provider>
+            </div>
+          </ReactQueryProvider>
         </StoreProvider>
       </body>
     </html>
