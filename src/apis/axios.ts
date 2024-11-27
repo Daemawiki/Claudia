b9a9da3c8c43fe833c48c7b535a4d3a1395ea829
ojinikim <ojinikim@gmail.com>
2024-11-27 15:46:38 +0900

    chore :: 서버 url 변경

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,8 @@
-import { BASE_URL } from './env';
+import { BASE_URL } from "./env";
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: 'https://daemawiki-server.xquare.app/',
+  baseURL: "https://daemawiki-server-stag.xquare.app/",
   timeout: 10000,
-  withCredentials: true, 
+  withCredentials: true,
 });
