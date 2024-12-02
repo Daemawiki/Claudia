75b503462182d77764465464a09a6d667a9a829a
ojinikim <ojinikim@gmail.com>
2024-12-02 23:43:30 +0900

    chore :: 404 페이지 화면 가운데 정렬

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function NotFound() {
   const router = useRouter();
   return (
-    <div>
+    <div className="w-full h-screen flex items-center justify-center">
       <p>돌아가.</p>
       <button onClick={() => router.back()}>뒤로 가기</button>
     </div>
