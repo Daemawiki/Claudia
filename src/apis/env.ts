4444d1f37772deb6b1b5cba0bba19ddb4ee959c1
ori0o0p <hello@seungwon.me>
2026-02-14 22:28:55 +0900

    chore: clean up apis lint violations

--- src/apis/env.ts
+++ src/apis/env.ts
@@ -1,9 +1,10 @@
 import process from "process";
 
 const isLocalhost = window.location.href.includes("localhost");
-const isStag = window.location.href.includes("stag");
 
-export const COOKIE_DOMAIN = isLocalhost ? "localhost" : "daemawiki-server.xquare.app";
+export const COOKIE_DOMAIN = isLocalhost
+  ? "localhost"
+  : "daemawiki-server.xquare.app";
 
-export const BASE_URL = process.env.BASE_URL;
+export const { BASE_URL } = process.env;
 export const SERVER_URL = process.env.SETVER_URL;
