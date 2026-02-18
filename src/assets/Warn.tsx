351cd3365238fce4410f4b408665b437306da7e3
ori0o0p <hello@seungwon.me>
2026-02-18 19:57:57 +0900

    chore :: assets 상태 아이콘 lint 오류 정리

--- src/assets/Warn.tsx
+++ src/assets/Warn.tsx
@@ -4,7 +4,7 @@ interface PropsType {
   className?: string;
 }
 
-export const Warn = ({ size = 24, onClick, className = "" }: PropsType) => {
+function Warn({ size = 24, onClick, className = "" }: PropsType) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -24,4 +24,13 @@ export const Warn = ({ size = 24, onClick, className = "" }: PropsType) => {
       />
     </svg>
   );
+}
+
+Warn.defaultProps = {
+  size: 24,
+  onClick: undefined,
+  className: "",
 };
+
+export { Warn };
+export default Warn;
