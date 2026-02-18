ccac9bf84a857b1f6d549f70e1c3277490dcd85c
ori0o0p <hello@seungwon.me>
2026-02-18 12:44:43 +0900

    fix :: SearchInput/Sidebar 타입 호환성 보강

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,16 +3,36 @@ import React from "react";
 
 interface InputProps {
   placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  onSubmit?: () => void;
 }
 
-export const SearchInput = ({ placeholder }: InputProps) => {
+export const SearchInput = ({
+  placeholder,
+  value,
+  onChange,
+  onSubmit,
+}: InputProps) => {
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit?.();
+  };
+
   return (
-    <div className="flex focus-within:bg-white min-h-9 transition-all px-3 py-2 items-center w-full gap-2 overflow-hidden border rounded-md bg-gray50 border-gray200">
+    <form
+      onSubmit={submitHandler}
+      className="flex focus-within:bg-white min-h-9 transition-all px-3 py-2 items-center w-full gap-2 overflow-hidden border rounded-md bg-gray50 border-gray200"
+    >
       <input
+        value={value}
+        onChange={event => onChange?.(event.target.value)}
         placeholder={placeholder}
         className="text-black placeholder:text-gray400 bg-transparent text-medium16 w-full"
       />
-      <Search size={20} className="text-gray400" />
-    </div>
+      <button type="submit" className="flex">
+        <Search size={20} className="text-gray400" />
+      </button>
+    </form>
   );
 };
